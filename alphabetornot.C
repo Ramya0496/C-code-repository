3e14fb2a9f84954f333c67f5b64ce113367f1a8e
Ramya0496 <ramya0496@gmail.com>
2016-10-05 21:08:56 +0530

    Create alphabetornot.C

---
+++ alphabetornot.C
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void) {
+	// your code goes here
+	char ch;
+	scanf("%c",&ch);
+	if(ch>=65 && ch<=90||ch>=97 && ch<=122)
+	{
+		printf("alphabet");
+	}
+	else
+	{
+		printf("not alphabet");
+	}
+	return 0;
+}
